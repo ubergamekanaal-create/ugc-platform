f7ae338aeebb4c29ffbc47a13e94e6222ba588d5
princekamboj7500 <princekamboj7500@gmail.com>
2026-05-13 13:59:16 +0530

    fix: env variable

--- app/api/team/invite/route.ts
+++ app/api/team/invite/route.ts
@@ -4,7 +4,7 @@ import { createClient } from "@/lib/supabase/server";
 
 import { Resend } from "resend";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+const resend = new Resend(process.env.RESEND_API_KEY!);
 export async function POST(req: Request) {
   const supabase = await createClient();
   const admin = createAdminClient();
